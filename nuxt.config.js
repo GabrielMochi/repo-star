858d9e2b075f27e71041bdb03ea7cdabc62ec06c
Gabriel Mochi <gmochi56@icloud.com>
2020-08-24 04:50:29 -0300

    feat: :arrow_up: dependencies upgraded

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,8 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | Repo Star',
+    title: 'App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -47,6 +47,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxtjs/dotenv',
     '@nuxt/typescript-build',
     '@nuxtjs/vuetify'
   ],
@@ -69,18 +70,24 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          background: colors.amber.lighten1,
+          primary: colors.amber.lighten1,
           accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          secondary: colors.grey.darken4,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
+      },
+      options: {
+        customProperties: true
       }
+    },
+    icons: {
+      iconfont: 'mdiSvg'
     }
   },
   /*
@@ -88,5 +95,15 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+  },
+  serverMiddleware: ['~/api/index.ts'],
+  env: {
+    githubOauthAppsClientId:
+      process.env.GITHUB_OAUTH_APPS_CLIENT_ID ||
+      '01427018ac1c7e886986',
+
+    githubLoginOauthAuthorizeEndpoint:
+      process.env.GITHUB_LOGIN_OAUTH_AUTHORIZE_ENDPOINT ||
+      'https://github.com/login/oauth/authorize'
   }
 }
