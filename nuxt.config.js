79c583e15a6325112b329f3233319665a8c8e658
Gabriel Mochi <gmochi56@icloud.com>
2020-08-25 01:42:18 -0300

    ci: :construction_worker: heroku deploy filed added

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,10 +97,6 @@ export default {
   build: {
   },
   serverMiddleware: ['~/api/index.ts'],
-  server: {
-    host: process.env.HOST || 'localhost',
-    port: process.env.PORT || '3000'
-  },
   env: {
     githubOauthAppsClientId:
       process.env.GITHUB_OAUTH_APPS_CLIENT_ID ||
