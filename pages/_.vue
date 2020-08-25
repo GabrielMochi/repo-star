5ee560535c7f52c86d182fe39db2e333b3262c58
Gabriel Mochi <gmochi56@icloud.com>
2020-08-25 01:29:35 -0300

    feat: :egg: not found page improved

--- pages/_.vue
+++ pages/_.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-container>
-    error
+  <v-container fluid class="fill-height justify-center">
+    <div class="d-block text-center">
+      <h1>This page never existed.</h1>
+      <v-btn to="/" outlined class="mt-8">
+        go to home
+      </v-btn>
+    </div>
   </v-container>
 </template>
